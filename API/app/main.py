9eae7ad4bf24d47267cbb1d773b0ccfa8a3589c4
Pablo Peramato <167205685+PabloPeramato@users.noreply.github.com>
2025-07-08 17:56:31 +0200

    Minimun Update main.py

--- API/app/main.py
+++ API/app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 # Middleware CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[ORIGINS],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
