cf22ba899d2e60626f195bc19ff16e6f8e3057ad
Pablo Peramato <167205685+PabloPeramato@users.noreply.github.com>
2025-07-08 18:40:57 +0200

    Update main.py

--- API/app/main.py
+++ API/app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 # Middleware CORS 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ORIGINS],
+    allow_origins=ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
